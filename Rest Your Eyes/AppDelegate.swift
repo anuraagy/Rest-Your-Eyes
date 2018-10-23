fd94488a5c4e660611e303463ce3aa491dd091a4
Anuraag Yachamaneni <ayachama@purdue.edu>
2018-10-23 13:48:27 -0400

    Create my first simple swift app

--- Rest Your Eyes/AppDelegate.swift
+++ Rest Your Eyes/AppDelegate.swift
@@ -12,7 +12,7 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    var timer : Timer!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -21,7 +21,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    @IBAction func notifyUser(sender: NSButton) {
+        let path = "/usr/bin/osascript";
+        let arguments = ["-e", "\'display notification \"Lorem ipsum dolor sit amet\" with title \"Title\"\'"]
+        let task = Process.launchedProcess(launchPath: path, arguments: arguments)
+        task.waitUntilExit()
+    }
+    
+    @IBAction func stopNotifying(sender: NSButton) {
+        if(timer != nil) {
+            timer.invalidate()
+            sender.isEnabled = false
+        }
+    }
+    
+    @IBAction func startNotifying(sender: NSButton) {
+        if(timer == nil) {
+            timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(AppDelegate.sendMessage), userInfo: nil, repeats: true)
+            sender.isEnabled = false
+        }
+    }
 
-
+    @IBAction func notify(_ sender: NSButton) {
+       
+    }
+    
+    @objc func sendMessage() {
+        let path = "/usr/bin/osascript";
+        let notification = "display notification \"Look up and rest your eyes\" with title \"Twenty minutes have passed\""
+        
+        let arguments = ["-e", notification];
+        let task = Process.launchedProcess(launchPath: path, arguments: arguments)
+        task.waitUntilExit()
+    }
+    
 }
 
